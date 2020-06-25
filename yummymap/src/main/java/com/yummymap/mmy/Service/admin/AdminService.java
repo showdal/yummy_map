d267dec2ffdf1d2c15d55d2e72fc3e063c465360
aktmzk112 <aktmzk112@gmail.com>
2020-06-25 18:20:23 +0900

    commit

--- yummymap/src/main/java/com/yummymap/mmy/Service/admin/AdminService.java
+++ yummymap/src/main/java/com/yummymap/mmy/Service/admin/AdminService.java
@@ -1,13 +1,34 @@
 package com.yummymap.mmy.Service.admin;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
+    static{
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        System.setProperty("current.date", dateFormat.format(new Date()));
+    }
+    
+	private  Logger logger = LoggerFactory.getLogger(AdminService.class);
 	public void PublicKeySrvc(HttpServletRequest req) {
+
+		System.out.println(System.getProperty("current.date")+ " ###################################################");
+		String ip = req.getHeader("X-FORWARDED-FOR");
+		if(ip == null) {
+			ip = req.getRemoteAddr();
+		}
+		Date time = new Date();
+		SimpleDateFormat form = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String date = form.format(time);
 		
+		logger.info("{} {} {}",date,ip," - 회원가입");
 		System.out.println("서비스~~~~~");
 	}
 }
--- yummymap/src/main/java/com/yummymap/mmy/dao/TxtDAO.java
+++ yummymap/src/main/java/com/yummymap/mmy/dao/TxtDAO.java
@@ -4,6 +4,7 @@ import com.yummymap.mmy.vo.txt.TxtVO;
 import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 public class TxtDAO {
 	@Autowired 
