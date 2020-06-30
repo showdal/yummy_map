9f9436ef0ec001c1538bfbf98fbbc96ec856a336
showdal <60916393+showdal@users.noreply.github.com>
2020-06-30 12:38:44 +0900

    Update write.js

--- yummymap/src/main/webapp/resources/js/txt/write.js
+++ yummymap/src/main/webapp/resources/js/txt/write.js
@@ -14,23 +14,32 @@ function setNum(cnt) {
 				$('#logout').show();
 				$('#mypage').attr('href', '/yummymap/member/member.mmy');
 			}
+			
+		$('#file').change(function(e){
+			var file = e.target.files;
+			console.log(file);
+		});
+		
 		$('#submitBtn').click(function() {
 			// 데이터 유효성 체크
 			let title_tag = $('#title').val();
-			if(title_tag.length >20) {
+			if(title_tag.length > 30) {
 				alert('제목의 길이를 초과했습니다.');
 				return;
 			}
 			let mid = $('#mid').val();
 			let select = $('#catno').val();
-//			alert(mid);
-//			alert(select);
+			let title = $('#title').val();
+			let mtxt = $('#mtxt').val();
+			
 			if(!select){
 				alert('카테고리를 선택 하세요!');
 				return;
 			}
+			$('#frm').attr('enctype','multipart/form-data');
 			$('#frm').submit();
 		});
+		
 		$('#exitBtn').click(function() {
 			$(location).attr('href', '/yummymap/txt/list.mmy');
 		});
